16e80e7dd2a4668b66338361fe2661b7a6612e6e
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-16 12:57:11 +0300

    refactor: replace hashmaps to lists

--- src/main/java/backend/academy/hangman/AnimalWordsCollection.java
+++ src/main/java/backend/academy/hangman/AnimalWordsCollection.java
@@ -1,43 +1,60 @@
 package backend.academy.hangman;
 
+import org.checkerframework.checker.units.qual.A;
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class AnimalWordsCollection {
-    private final HashMap<String, AnimalWord> animal_words_collection_easy;
-    private final HashMap<String, AnimalWord> animal_words_collection_medium;
-    private final HashMap<String, AnimalWord> animal_words_collection_hard;
+    private final List<AnimalWord> animal_words_collection_easy;
+    private final List<AnimalWord> animal_words_collection_medium;
+    private final List<AnimalWord> animal_words_collection_hard;
 
     public AnimalWordsCollection() {
-        animal_words_collection_easy = new HashMap<>();
-        animal_words_collection_medium = new HashMap<>();
-        animal_words_collection_hard = new HashMap<>();
+        animal_words_collection_easy = new ArrayList<>();
+        animal_words_collection_medium = new ArrayList<>();
+        animal_words_collection_hard = new ArrayList<>();
         setAnimal_words_collection_easy();
+        setAnimal_words_collection_medium();
+        setAnimal_words_collection_hard();
     }
 
     private void setAnimal_words_collection_easy() {
         AnimalWord cow = new AnimalWord("Cow", "Farm animal");
         AnimalWord hyena = new AnimalWord("Hyena", "Carnivorous animal");
         AnimalWord giraffe = new AnimalWord("Giraffe", "Animal with the longest neck");
-        animal_words_collection_easy.put("Cow", cow);
-        animal_words_collection_easy.put("Hyena", hyena);
-        animal_words_collection_easy.put("Giraffe", giraffe);
+        AnimalWord whale = new AnimalWord("Whale", "The animal lives in the ocean");
+        animal_words_collection_easy.add(cow);
+        animal_words_collection_easy.add(hyena);
+        animal_words_collection_easy.add(giraffe);
+        animal_words_collection_easy.add(whale);
     }
 
     private void setAnimal_words_collection_medium() {
         AnimalWord capybara = new AnimalWord("Capybara", "The friendliest animal");
         AnimalWord raccoon = new AnimalWord("Raccoon", "Distinctive dark markings on their faces");
         AnimalWord peacock = new AnimalWord("Peacock", "Animal with sea green `eyes` on its tail");
-        animal_words_collection_medium.put("Capybara", capybara);
-        animal_words_collection_medium.put("Raccoon", raccoon);
-        animal_words_collection_medium.put("Peacock", peacock);
+        animal_words_collection_medium.add(capybara);
+        animal_words_collection_medium.add(raccoon);
+        animal_words_collection_medium.add(peacock);
     }
 
     private void setAnimal_words_collection_hard() {
         AnimalWord chinchilla = new AnimalWord("Chinchilla", "Cute animal");
-        animal_words_collection_hard.put("Chinchilla", chinchilla);
+        animal_words_collection_hard.add(chinchilla);
     }
 
-    public HashMap<String, AnimalWord> getAnimal_words_collection_easy() {
-        return animal_words_collection_easy;
+    public AnimalWord getRandomEasyAnimalWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return animal_words_collection_easy.get(secureRandom.nextInt(animal_words_collection_easy.size()));
+    }
+    public AnimalWord getRandomMediumAnimalWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return animal_words_collection_medium.get(secureRandom.nextInt(animal_words_collection_medium.size()));
+    }
+    public AnimalWord getRandomHardAnimalWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return animal_words_collection_hard.get(secureRandom.nextInt(animal_words_collection_hard.size()));
     }
 }
