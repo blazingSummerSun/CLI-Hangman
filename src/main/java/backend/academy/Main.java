b9c5953f132b85fd5d2ed0a48b032af2a6ac0c2e
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-16 12:58:21 +0300

    feat: create entry point to the game

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,12 @@
 package backend.academy;
 
+import backend.academy.hangman.HangmanGame;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        HangmanGame hangmanGame = new HangmanGame(System.in, System.out);
+//        hangmanGame.launchGame(System.in, System.out);
     }
 }
