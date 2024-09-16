606612ca60c58e1586325a65d206988f414e4156
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-16 12:56:33 +0300

    refactor: rename clue to hint in word class

--- src/main/java/backend/academy/hangman/Word.java
+++ src/main/java/backend/academy/hangman/Word.java
@@ -2,15 +2,15 @@ package backend.academy.hangman;
 
 public abstract class Word {
     protected String word;
-    protected String clue;
-    public Word(String word, String clue) {
+    protected String hint;
+    public Word(String word, String hint) {
         this.word = word;
-        this.clue = clue;
+        this.hint = hint;
     }
     public String getWord() {
         return word;
     }
-    public String getClue() {
-        return clue;
+    public String getHint() {
+        return hint;
     }
 }
