d8811d6e2ad8c045b3dc4c390c6bc4a9507ce343
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-16 22:03:45 +0300

    refactor: delete extra empty spaces

--- src/main/java/backend/academy/hangman/WordsCollection.java
+++ src/main/java/backend/academy/hangman/WordsCollection.java
@@ -15,7 +15,6 @@ public class WordsCollection {
     private final List<LocationWord> location_words_collection_medium;
     private final List<LocationWord> location_words_collection_hard;
 
-
     public WordsCollection() {
         animal_words_collection_easy = new ArrayList<>();
         animal_words_collection_medium = new ArrayList<>();
@@ -61,28 +60,34 @@ public class WordsCollection {
         AnimalWord chinchilla = new AnimalWord("chinchilla", "Cute animal");
         animal_words_collection_hard.add(chinchilla);
     }
+
     private void setDevice_words_collection_easy() {
         DeviceWord mouse = new DeviceWord("mouse", "Moves a cursor within the screen");
         DeviceWord screen = new DeviceWord("screen", "Displays some information from a user");
         device_words_collection_easy.add(mouse);
         device_words_collection_easy.add(screen);
     }
+
     private void setDevice_words_collection_medium() {
         DeviceWord headphones = new DeviceWord("headphones", "Sound device");
         device_words_collection_medium.add(headphones);
     }
+
     private void setDevice_words_collection_hard() {
         DeviceWord projector = new DeviceWord("projector", "Displays information on the wall");
         device_words_collection_hard.add(projector);
     }
+
     private void setLocation_words_collection_easy() {
         LocationWord everest = new LocationWord("everest", "The highest mountain in the world");
         location_words_collection_easy.add(everest);
     }
+
     private void setLocation_words_collection_medium() {
         LocationWord france = new LocationWord("france", "The love's country");
         location_words_collection_medium.add(france);
     }
+
     private void setLocation_words_collection_hard() {
         LocationWord madagascar = new LocationWord("madagascar", "The place called by the cartoon");
         location_words_collection_hard.add(madagascar);
@@ -92,34 +97,42 @@ public class WordsCollection {
         SecureRandom secureRandom = new SecureRandom();
         return animal_words_collection_easy.get(secureRandom.nextInt(animal_words_collection_easy.size()));
     }
+
     public AnimalWord getRandomMediumAnimalWord() {
         SecureRandom secureRandom = new SecureRandom();
         return animal_words_collection_medium.get(secureRandom.nextInt(animal_words_collection_medium.size()));
     }
+
     public AnimalWord getRandomHardAnimalWord() {
         SecureRandom secureRandom = new SecureRandom();
         return animal_words_collection_hard.get(secureRandom.nextInt(animal_words_collection_hard.size()));
     }
+
     public DeviceWord getRandomEasyDeviceWord() {
         SecureRandom secureRandom = new SecureRandom();
         return device_words_collection_easy.get(secureRandom.nextInt(device_words_collection_easy.size()));
     }
+
     public DeviceWord getRandomMediumDeviceWord() {
         SecureRandom secureRandom = new SecureRandom();
         return device_words_collection_medium.get(secureRandom.nextInt(device_words_collection_medium.size()));
     }
+
     public DeviceWord getRandomHardDeviceWord() {
         SecureRandom secureRandom = new SecureRandom();
         return device_words_collection_hard.get(secureRandom.nextInt(device_words_collection_hard.size()));
     }
+
     public LocationWord getRandomEasyLocationWord() {
         SecureRandom secureRandom = new SecureRandom();
         return location_words_collection_easy.get(secureRandom.nextInt(location_words_collection_easy.size()));
     }
+
     public LocationWord getRandomMediumLocationWord() {
         SecureRandom secureRandom = new SecureRandom();
         return location_words_collection_medium.get(secureRandom.nextInt(location_words_collection_medium.size()));
     }
+
     public LocationWord getRandomHardLocationWord() {
         SecureRandom secureRandom = new SecureRandom();
         return location_words_collection_hard.get(secureRandom.nextInt(location_words_collection_hard.size()));
