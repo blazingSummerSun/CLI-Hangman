564ea559b4aa183c5ab5aa30a9ec367fdd43e36e
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-16 13:19:31 +0300

    refactor: rename AnimalWordsCollection class to WordsCollection and add device words init

--- src/main/java/backend/academy/hangman/WordsCollection.java
+++ src/main/java/backend/academy/hangman/WordsCollection.java
@@ -1,23 +1,31 @@
 package backend.academy.hangman;
 
-import org.checkerframework.checker.units.qual.A;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-public class AnimalWordsCollection {
+public class WordsCollection {
     private final List<AnimalWord> animal_words_collection_easy;
     private final List<AnimalWord> animal_words_collection_medium;
     private final List<AnimalWord> animal_words_collection_hard;
+    private final List<DeviceWord> device_words_collection_easy;
+    private final List<DeviceWord> device_words_collection_medium;
+    private final List<DeviceWord> device_words_collection_hard;
 
-    public AnimalWordsCollection() {
+
+    public WordsCollection() {
         animal_words_collection_easy = new ArrayList<>();
         animal_words_collection_medium = new ArrayList<>();
         animal_words_collection_hard = new ArrayList<>();
+        device_words_collection_easy = new ArrayList<>();
+        device_words_collection_medium = new ArrayList<>();
+        device_words_collection_hard = new ArrayList<>();
         setAnimal_words_collection_easy();
         setAnimal_words_collection_medium();
         setAnimal_words_collection_hard();
+        setDevice_words_collection_easy();
+        setDevice_words_collection_medium();
+        setDevice_words_collection_hard();
     }
 
     private void setAnimal_words_collection_easy() {
@@ -44,6 +52,20 @@ public class AnimalWordsCollection {
         AnimalWord chinchilla = new AnimalWord("Chinchilla", "Cute animal");
         animal_words_collection_hard.add(chinchilla);
     }
+    private void setDevice_words_collection_easy() {
+        DeviceWord mouse = new DeviceWord("Mouse", "Moves a cursor within the screen");
+        DeviceWord screen = new DeviceWord("Screen", "Displays some information from a user");
+        device_words_collection_easy.add(mouse);
+        device_words_collection_easy.add(screen);
+    }
+    private void setDevice_words_collection_medium() {
+        DeviceWord headphones = new DeviceWord("Headphones", "Sound device");
+        device_words_collection_medium.add(headphones);
+    }
+    private void setDevice_words_collection_hard() {
+        DeviceWord projector = new DeviceWord("Projector", "Displays information on the wall");
+        device_words_collection_hard.add(projector);
+    }
 
     public AnimalWord getRandomEasyAnimalWord() {
         SecureRandom secureRandom = new SecureRandom();
@@ -56,5 +78,17 @@ public class AnimalWordsCollection {
     public AnimalWord getRandomHardAnimalWord() {
         SecureRandom secureRandom = new SecureRandom();
         return animal_words_collection_hard.get(secureRandom.nextInt(animal_words_collection_hard.size()));
+    }
+    public DeviceWord getRandomEasyDeviceWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return device_words_collection_easy.get(secureRandom.nextInt(device_words_collection_easy.size()));
+    }
+    public DeviceWord getRandomMediumDeviceWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return device_words_collection_medium.get(secureRandom.nextInt(device_words_collection_medium.size()));
+    }
+    public DeviceWord getRandomHardDeviceWord() {
+        SecureRandom secureRandom = new SecureRandom();
+        return device_words_collection_hard.get(secureRandom.nextInt(device_words_collection_hard.size()));
     }
 }
