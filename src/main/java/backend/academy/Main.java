3c0408a388e83ef5f176a32ddb441d3e556f45ab
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-09-20 13:12:26 +0300

    refactor: delete input and output arguments in the game constructor

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -6,7 +6,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
-        HangmanGame hangmanGame = new HangmanGame(System.in, System.out);
+        HangmanGame hangmanGame = new HangmanGame();
         hangmanGame.launchGame(System.in, System.out);
     }
 }
